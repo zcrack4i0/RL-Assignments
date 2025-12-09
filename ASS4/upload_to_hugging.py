bd5acf4c1f477d2c9c588a81a16ffb5cc86a3747
Mostafa Osama <Mostafa.Nassar03@eng-st.cu.edu.eg>
2025-12-09 23:37:38 +0200

    sd

---
+++ ASS4/upload_to_hugging.py
@@ -0,0 +1,36 @@
+import gymnasium as gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+
+from huggingface_sb3 import package_to_hub
+
+# PLACE the variables you've just defined two cells above
+# Define the name of the environment
+env_id = "LunarLander-v2"
+
+# TODO: Define the model architecture we used
+model_architecture = "PPO"
+
+## Define a repo_id
+## repo_id is the id of the model repository from the Hugging Face Hub (repo_id = {organization}/{repo_name} for instance ThomasSimonini/ppo-LunarLander-v2
+## CHANGE WITH YOUR REPO ID
+repo_id = "ThomasSimonini/ppo-LunarLander-v2"  # Change with your repo id, you can't push with mine 😄
+
+## Define the commit message
+commit_message = "Upload PPO LunarLander-v2 trained agent"
+
+# Create the evaluation env and set the render_mode="rgb_array"
+eval_env = DummyVecEnv([lambda: Monitor(gym.make(env_id, render_mode="rgb_array"))])
+
+# PLACE the package_to_hub function you've just filled here
+package_to_hub(
+    model=model,  # Our trained model
+    model_name=model_name,  # The name of our trained model
+    model_architecture=model_architecture,  # The model architecture we used: in our case PPO
+    env_id=env_id,  # Name of the environment
+    eval_env=eval_env,  # Evaluation Environment
+    repo_id=repo_id,  # id of the model repository from the Hugging Face Hub (repo_id = {organization}/{repo_name} for instance ThomasSimonini/ppo-LunarLander-v2
+    commit_message=commit_message,
+)
